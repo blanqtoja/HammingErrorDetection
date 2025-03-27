6a4c215c9529da6967c4b378c09a37f17178209f
blanqtoja <251544@edu.p.lodz.pl>
2025-03-27 18:59:25 +0100

    wczytanie z pliku

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 #include "./doubleError/doubleErrorDetection.h"
+#include "./singleError/singleErrorDetection.h"
 
 int main() {
 
@@ -19,9 +21,56 @@ int main() {
     std::cin >> option;
 
     if (option == '1') {
+        option = '0'; // reset option
+        do{
+            std::cout << "1) single error";
+            std::cout << "2) single/double error";
+            std::cout << "Enter an option: ";
 
+        } while(option != '1' && option != '2');
+
+        std::string message;
+        std::cout << "Enter a message: ";
+        std::cin >> message;
+        std::vector<bool> messange;
+        for (auto i : message) {
+            messange.push_back(i);
+        }
+
+        if(option == '1') {
+
+            singleErrorDetection(messange);
+        }
+        else if(option == '2'){
+            doubleErrorDetection(messange);
+        }
     }
     else if (option == '2') {
+
+        std::string message;
+        std::cout << "Enter a file name: ";
+        std::cin >> message;
+
+        std::ifstream file(message, std::ios::binary);
+        if (!file) {
+            std::cerr << "Nie można otworzyć pliku!" << std::endl;
+            return 1;
+        }
+
+        // Sprawdzenie rozmiaru pliku
+        std::streamsize fileSize = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        std::cout << "Rozmiar pliku: " << fileSize << " bajtów" << std::endl;
+
+
+//        // Wyświetlenie wartości bajtów jako liczby
+//        for (char c : buffer) {
+//            std::cout << std::hex << static_cast<int>(static_cast<unsigned char>(c)) << " ";
+//        }
+
+        file.close();
+
 
     }
 
